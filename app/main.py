6c2c38bb4f9e236d09b9d34e87eaf2c62fe8d534
LSWarss <ls.warss98@gmail.com>
2022-11-22 21:23:42 +0100

    feat(rest-api): Added basic image structure, and refactored rest of the endpoints

--- app/main.py
+++ app/main.py
@@ -1,17 +1,62 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, responses, HTTPException
+from pydantic import BaseModel, Field
+from uuid import UUID, uuid4
 from ImageProcessing import ears_drawing, face_recognition
 from Resources.Paths import test_photo
+
+app = FastAPI()
 
 faces = face_recognition.recognize_faces(test_photo)
 photo = ears_drawing.draw_ears(faces, test_photo)
 photo.show()
 
-app = FastAPI()
+
+class Image(BaseModel):
+    id: UUID
+    url: str
+
+
+IMAGES = []
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/api/image/{id}")
+async def get_image(id: UUID):
+    for image in IMAGES:
+        if image.id == id:
+            return image
+    raise HTTPException(
+        status_code=404,
+        detail=f"Image with ID: '{id}': Does not exist"
+    )
+
+
+@app.get("/api/images")
+async def get_image_list():
+    return IMAGES
+
+
+@app.post("/api/image")
+async def post_image(file: UploadFile = File(...)):
+    image = Image(id=uuid4(), url=file.filename)
+    IMAGES.append(image)
+    return image
+
+
+@app.delete("/api/image/{id}", status_code=204)
+async def delete_image(id: UUID):
+    for index, image in enumerate(IMAGES):
+        if image == id:
+            IMAGES.pop(index)
+            return f"Image with ID: {id} deleted"
+    raise HTTPException(
+        status_code=404,
+        detail=f"Image with ID: '{id}': Does not exist"
+    )
 
 
 @app.get("/api/processing")
