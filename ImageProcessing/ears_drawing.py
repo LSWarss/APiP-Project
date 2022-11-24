6a874394794706790e394ae42046710fa9e35ae6
Piotr Sztefko <p.sztefko@gmail.com>
2022-11-24 14:47:18 +0100

    refactor(image processing): resolved pr comments

--- ImageProcessing/ears_drawing.py
+++ ImageProcessing/ears_drawing.py
@@ -1,16 +1,23 @@
 from PIL import Image, ImageOps
 from Resources.Paths import shrek_ear, output_path
+from Resources import Constants
 
 
 def draw_ears(faces, image_path):
     photo = Image.open(image_path)
     ear = Image.open(shrek_ear)
-    right_ear = ear.resize((30, 30))
-    left_ear = ImageOps.mirror(right_ear)
 
     for (x, y, w, h) in faces:
-        photo.paste(right_ear, (int(x + w * 2 / 3), y - h), right_ear)
-        photo.paste(left_ear, (int(x - w * 3 / 10), y - h), left_ear)
+        right_ear = ear.resize((round(w / Constants.heigth_divider), round(h / Constants.heigth_divider)))
+        left_ear = ImageOps.mirror(right_ear)
+        photo.paste(right_ear,
+                    (int(x + w * Constants.right_width_multiplier),
+                     y - round(h / Constants.heigth_divider)),
+                    right_ear)
+        photo.paste(left_ear,
+                    (int(x - w * Constants.left_width_multiplier),
+                     y - round(h / Constants.heigth_divider)),
+                    left_ear)
 
     photo.save(output_path)
 
