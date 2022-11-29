42734f3d8b8f19f6d999f32b304d91c21e841a64
LSWarss <ls.warss98@gmail.com>
2022-11-29 20:39:49 +0100

    feat(rest-api): Added proccessing image endpoint and saving the images and downloading

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException, Depends
+from fastapi.responses import FileResponse
 from app.image_processing import ears_drawing, face_recognition
 from app.resources.paths import test_photo
 from sqlalchemy.orm import Session
 from app import models
 from app.database import session_local, engine
+from os import getcwd, path
 
 app = FastAPI()
 models.base.metadata.create_all(bind=engine)
@@ -15,11 +17,6 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-faces = face_recognition.recognize_faces(test_photo)
-photo = ears_drawing.draw_ears(faces, test_photo)
-photo.show()
 
 
 @app.get("/")
@@ -42,6 +39,24 @@ async def get_image(id: int, db: Session = Depends(get_db)):
     return image
 
 
+@app.get("/api/image/download/{id}")
+async def get_image(id: int, db: Session = Depends(get_db)):
+    image_model = db.query(models.Images).filter(
+        models.Images.id == id).first()
+
+    if image_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Image with ID: '{id}': Does not exist"
+        )
+
+    image = models.Image(url=image_model.url)
+
+    return FileResponse(path=getcwd() + "/" + image.url,
+                        media_type='application/octet-stream',
+                        filename=image.url)
+
+
 @app.get("/api/images")
 async def get_image_list(db: Session = Depends(get_db)):
     return db.query(models.Images).all()
@@ -50,12 +65,41 @@ async def get_image_list(db: Session = Depends(get_db)):
 @app.post("/api/image")
 async def post_image(file: UploadFile = File(...),
                      db: Session = Depends(get_db)):
+    with open(file.filename, 'wb') as image:
+        content = await file.read()
+        image.write(content)
+        image.close()
+
     image_model = models.Images()
     image_model.url = file.filename
     db.add(image_model)
     db.commit()
 
     return image_model
+
+
+@app.post("/api/processing")
+async def proccess_image(file: UploadFile = File(...),
+                         db: Session = Depends(get_db)):
+    with open(file.filename, 'wb') as image:
+        content = await file.read()
+        image.write(content)
+        image.close()
+
+    faces = face_recognition.recognize_faces(file.filename)
+    photo = ears_drawing.draw_ears(faces, file.filename)
+    filename, file_extension = path.splitext(file.filename)
+    process_filename = filename + "_shreked" + file_extension
+    photo.save(process_filename)
+
+    image_model = models.Images()
+    image_model.url = process_filename
+    db.add(image_model)
+    db.commit()
+
+    return FileResponse(path=getcwd() + "/" + process_filename,
+                        media_type='application/octet-stream',
+                        filename=process_filename)
 
 
 @app.delete("/api/image/{id}", status_code=204)
